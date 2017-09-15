99b6173ff8cf9d0e5ec3f8bb7a141cd3209d7fa4
Silvio Jaureguibehere <silvio.jaureguibehere@gmail.com>
2017-09-15 13:34:32 -0300

    version 0.1.0 sdk 0.3.0

---
+++ PlaywireMopubAdapter.framework/Headers/PlaywireMopubAdapter.h
@@ -0,0 +1,20 @@
+//
+//  PlaywireMopubAdapter.h
+//  PlaywireMopubAdapter
+//
+//  Created by Silvio Jaureguibehere on 8/17/17.
+//  Copyright © 2017 Playwire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlaywireMopubAdapter.
+FOUNDATION_EXPORT double PlaywireMopubAdapterVersionNumber;
+
+//! Project version string for PlaywireMopubAdapter.
+FOUNDATION_EXPORT const unsigned char PlaywireMopubAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlaywireMopubAdapter/PublicHeader.h>
+
+#import "PWMopubCustomEventInterstitial.h"
+
